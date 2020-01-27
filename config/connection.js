ad9398783af4c403fcacd4a7ba983485fc5ba3e6
Serina Harris <sbharris1977@gmail.com>
2020-01-27 13:03:28 -0600

    adding in jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 //Set up connection
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    //socketPath: "mysql-socket-path",
-    password: "Serina",
-    database: "burgers_db"
+if (process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {  
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        //socketPath: "mysql-socket-path",
+        password: "Serina",
+        database: "burgers_db"
 });
+};
 
 //make connection
 connection.connect(function(err) {
